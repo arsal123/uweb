c4e1eaa8c1c691e8e1487a549965c79c127ae9c2
Arsalan <arsalan.siddiqui@philips.com>
2017-07-20 01:31:16 -0400

    try to auth

--- public/js/services/main-service.js
+++ public/js/services/main-service.js
@@ -6,43 +6,60 @@
             '$http',
             '$q',
             '$log',
-            function($http, $q, $log) {
+            function ($http, $q, $log) {
                 let _this = this;
                 const BASE_URL = 'http://' + window.location.host;
                 const DB_URL = BASE_URL + '/db/';
 
                 _this.BASE_URL = BASE_URL;
 
-                _this.getCategories = function(){
+                // Need to fix this method
+                var doAuth = function () {
+                    $http({
+                        method: 'POST',
+                        url: DB_URL + 'auth',
+                        body: {
+                            username: 'admin1',
+                            password: 'siddiqui1'
+                        }
+                    })
+                        .then(function (res) {
+                            $log.debug(res.data);
+                        });
+                }
+
+                _this.getCategories = function () {
                     return $http({
                         method: 'GET',
-                        url: DB_URL+'category'
+                        url: DB_URL + 'category'
                     })
-                    .then(function(res){
-                        return res.data;    
-                    })
+                        .then(function (res) {
+                            return res.data;
+                        })
                 }
 
-                let getItems = function(){
-                    return $http.get(DB_URL+'item')
-                    .then(function(res){
-                        return res.data;                        
-                    })
+                let getItems = function () {
+                    return $http.get(DB_URL + 'item')
+                        .then(function (res) {
+                            return res.data;
+                        })
                 }
 
-                _this.getThirdItem = function(){
+                _this.getThirdItem = function () {
                     let itemPromise = getItems();
-                    return getItems().then(function(items){
+                    return getItems().then(function (items) {
                         return items[0];
                     });
                 }
 
-                _this.getNewArrivalItems = function(){
+                _this.getNewArrivalItems = function () {
                     let itemPromise = getItems();
-                    return getItems().then(function(items){
+                    return getItems().then(function (items) {
                         return items[1];
                     });
                 }
+
+                // doAuth();
 
                 return _this;
             }]);
