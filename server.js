f9a22f5c45a5e7928dd723ddc6faa8434c24eded
Arsalan <arsalan.siddiqui@philips.com>
2017-07-22 14:03:17 -0400

    adding functions needed for ssl

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var express = require("express");
-var app     = express();
+'use strict'
+
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
 // var path    = require("path");
 var colors = require("colors");
 var httpProxy = require('http-proxy');
@@ -9,6 +13,25 @@ const ShippingContr = require('./app/controllers/shippingController');
 var apiProxy = httpProxy.createProxyServer();
 const serverOne = 'http://13.58.6.10:3150';
 // const serverOne = 'http://localhost:3150';
+let setup = function (ssl) {
+  if (ssl && ssl.active) {
+    return {
+      key: fs.readFileSync(ssl.key),
+      cert: fs.readFileSync(ssl.certificate)
+    };
+  }
+}
+let start = function start(app, options) {
+  if (options)
+    return require('https').createServer(options, app);
+
+  return require('http').createServer(app);
+}
+
+let create = function (settings, app, cb) {
+  var options = setup(settings.ssl);
+  return start(app, options).listen(settings.port, cb);
+}
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/prod-imgs'));
@@ -16,24 +39,34 @@ app.use(express.static(__dirname + '/prod-imgs'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
   // console.log('TEST-PATH: '+__dirname+'/public/index.html')  
   // res.sendFile(path.join(__dirname + '/public/index.html'));
   res.sendFile('index.html');
   //__dirname : It will resolve to your project folder.
 });
 
-app.all('/db/*', function(req, res){
+app.all('/db/*', function (req, res) {
   // console.log(colors.bgMagenta('GOING TO DB SERVER with '+req.url));
-// since /db is 3 char so truncating first 3 characters 
+  // since /db is 3 char so truncating first 3 characters 
   req.url = req.url.substr(3);
-  console.log(colors.bgMagenta('Final URL: '+req.url));
-  
-  apiProxy.web(req, res, {target: serverOne});
+  console.log(colors.bgMagenta('Final URL: ' + req.url));
+
+  apiProxy.web(req, res, { target: serverOne });
 });
 
 app.get('/shipping-calc', ShippingContr.getRates);
 
-app.listen(3000);
+// app.listen(3000);
+let settings = {
+  ssl: {
+    active: false,
+    key: '',
+    certificate: '' 
+  },
+  port: 3000 //alt it can be port 443 for assl
+}
 
-console.log("Running at Port 3000".white);
+create(settings, app, function(){
+  console.log(("Running at Port " + settings.port).white);
+})
