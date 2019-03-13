28ba07b4e4e54d15de193428390e87e4261d5edd
M Arsalan Siddiqui <arsalansiddiqui@Ms-MacBook-Pro.local>
2019-03-13 09:37:39 -0400

    Added login page

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var app = express();
 // var path    = require("path");
 var colors = require("colors");
 var httpProxy = require('http-proxy');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'),
+    passport = require('passport'),
+    session = require('express-session');
 const ShippingContr = require('./app/controllers/shippingController');
 
 var apiProxy = httpProxy.createProxyServer();
@@ -34,6 +36,7 @@ let create = function (settings, app, cb) {
   return start(app, options).listen(settings.port, cb);
 }
 
+
 app.all('/ldb/*', function (req, res) {
   // console.log(colors.bgMagenta('GOING TO DB SERVER with '+req.url));
   // since /db is 3 char so truncating first 3 characters 
@@ -53,8 +56,43 @@ app.get('/item/', function (req, res) {
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/../shared-img/item'));
 
+require('./public/js/userController')(passport);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Require for passport 
+app.use(session({ 
+  secret: 'uwebdbappsession',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 3600000}
+})); // this should be hidden 
+app.use(passport.initialize());
+app.use(passport.session()); // creates a persistent login sessions
+
+app.all('/auth', passport.authenticate('local-login'), function(req, res){
+  console.log('Authentication at /auth successful');
+  res.redirect('/index2.html');  
+});
+
+app.use(function (req, res, next) {
+  console.log('Middleware Interrupt: ' + res.statusCode);
+  if (req.isAuthenticated() || req.url.includes('/shipping-calc') ){
+          
+          console.log('reqeust is authenticated');
+          next();
+
+  } else {
+      console.log('web visitor is NOT authenicated');
+      if(req.hostname === 'localhost') {
+        let finalUrl = 'http://' + req.headers.host + '/login.html';
+        console.log('Going to Localhost login: ' + finalUrl);
+
+        res.redirect(finalUrl);
+      }
+  }
+});
+
 
 // For getting rates for the package from Canada Post
 app.get('/shipping-calc', ShippingContr.getRates);
